8c97cb2cbcfdcad11bc779fb35ddbb3804a1beae
canilkum <canilkum@LIN66015116.corp.capgemini.com>
2022-01-08 21:32:22 +0530

    service registry

---
+++ service-registory/src/test/java/com/sk11/service/registory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk11.service.registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
